7e32746b0c9bfc1ec305d42918494d4afea855a7
joshua1b <joshua1b2823@gmail.com>
2018-07-28 01:56:46 +0900

    game.go: Add json tag for Game struct

--- game.go
+++ game.go
@@ -19,8 +19,8 @@ const (
 type Team int
 
 type Game struct {
-	Home     Team
-	Away     Team
-	Canceled bool
-	Score    [2]int
+	Home     Team   `json:"home"`
+	Away     Team   `json:"away"`
+	Canceled bool   `json:"canceled"`
+	Score    [2]int `json:"score"`
 }
